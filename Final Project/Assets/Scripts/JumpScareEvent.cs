973e6858d6727235b7d616f9a8d7f69ca53d6748
MajoretteKay <keolecki@gmail.com>
2020-05-03 15:21:32 -0400

    Final Trap finish

---
+++ Final Project/Assets/Scripts/JumpScareEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScareEvent : MonoBehaviour
+{
+    public Light[] lights;
+    public GameObject idol;
+    public GameObject scare;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Collider[] hitColliders = Physics.OverlapSphere(new Vector3(0f, 1.5f, 0f), 1.0f);
+         foreach(Collider i in hitColliders) {
+             if (i.tag == "Player") {
+                 Event();
+
+             }
+         }
+    }
+
+    void Event() {
+        foreach(Light light in lights) {
+            light.color = Color.red;
+        }
+        scare.active = true;
+        
+    }
+}
