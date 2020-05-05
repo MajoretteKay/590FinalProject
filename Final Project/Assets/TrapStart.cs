6830b7a9abe0b46c9d07eea725d8d91f5da053cb
W. Alan Patterson II <walanpat@live.unc.edu>
2020-05-05 16:08:18 -0400

    Added in the level I  made many a time ago
    In a land far far away, my teammates created 2 levels.  I William Alan Patterson the 2nd born of middle class nobility, have thus creataed the legendary third one.  Although I could not bass boost indiana jones's theme music, I could indeed create a riveting game to a even more exciting end to my Junior Year.

---
+++ Final Project/Assets/TrapStart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+  
+public class TrapStart : MonoBehaviour
+{
+
+    public GameObject soundtoPlay;
+    public bool alreadyPlayed = false;
+    public CharacterController player;
+    public GameObject deadwall;
+    public GameObject deadwall2;
+    // Start is called before the first frame update
+  
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter(Collider other){
+        // if(other == player.tag){
+            Destroy(deadwall);
+            Destroy(deadwall2);
+    //     }
+    if(!alreadyPlayed){
+        soundtoPlay.active = true;
+        alreadyPlayed=true;
+    }
+     }
+}
